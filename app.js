50524f2d707bea1092fc75b8a1c7e12efd6c9c91
paublancafortguardiola <137887899+paublancafortguardiola@users.noreply.github.com>
2023-09-11 22:43:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Imports
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // Requiring fs module in which
 // readFile function is defined.
